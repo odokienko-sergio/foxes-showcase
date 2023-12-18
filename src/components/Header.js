adaa4f1eeb2103a9ec1355a625410280d40f4dc4
Serhii Odokiienko <58706778+odokienko-sergio@users.noreply.github.com>
2023-12-18 15:35:41 +0200

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import React from 'react';
 function Header() {
     return (
         <div>
-            <h1>Our fox<span>es</span></h1>
+            <h1 className={"main-title"}>Our fox<span>es</span></h1>
             <div className="line"></div>
         </div>
     );
