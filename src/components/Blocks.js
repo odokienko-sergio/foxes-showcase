33f9edb457702f040ec56ba30be9e575e6a5e43c
Serhii Odokiienko <58706778+odokienko-sergio@users.noreply.github.com>
2023-12-19 11:09:38 +0200

    Update Blocks.js

--- src/components/Blocks.js
+++ src/components/Blocks.js
@@ -3,12 +3,12 @@
 import React from 'react';
 import Block from './Block';
 
-const Blocks = ({ blocksData, handleLikeClick, blocksOrder }) => {
+const Blocks = ({ blocksData, handleLikeClick, handleReadMoreClick, blocksOrder }) => {
     return (
         <div className="content">
             {blocksOrder.map((title, index) => {
                 const block = blocksData.find((b) => b.title === title);
-                return <Block key={index} block={block} handleLikeClick={handleLikeClick} index={index} />;
+                return <Block key={index} block={block} handleLikeClick={handleLikeClick} handleReadMoreClick={handleReadMoreClick} index={index} />;
             })}
         </div>
     );
